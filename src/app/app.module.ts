8a3222cb07d78105ad39d25b3889d3420da99646
mohankalyan <mohankalyan94@gmail.com>
2019-02-25 07:36:30 +0530

    nav bar update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { CarouselComponent } from './content/carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    CarouselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
