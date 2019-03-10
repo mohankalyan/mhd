5204e6801f758dc6f27c4964791555d01849950a
mohankalyan <mohankalyan94@gmail.com>
2019-03-10 23:23:03 +0530

    mobile nav bar update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +7,26 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  expandMenu:boolean=true;
+  currentSection='home';
+
   constructor() { }
 
+  public innerWidth: any;
   ngOnInit() {
+      this.setExpandMenu(window.innerWidth);
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.setExpandMenu(window.innerWidth);
   }
 
-  currentSection='home';
+  setExpandMenu(width){
+    if(width<768)
+      this.expandMenu=false;
+    else
+      this.expandMenu=true;
+  }
 
   isActive(id){
     return this.currentSection === id;
